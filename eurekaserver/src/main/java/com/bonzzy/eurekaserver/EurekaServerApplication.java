ed0643ccf31e0cafac4bac40e1a0a53c30a13882
lilongyun <932740514@qq.com>
2018-05-10 13:40:00 +0800

    新增eurekaserver

---
+++ eurekaserver/src/main/java/com/bonzzy/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.bonzzy.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by lilongyun on 2018/5/10.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(EurekaServerApplication.class);
+    }
+}
