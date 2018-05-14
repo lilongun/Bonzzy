0e66458d06372e9691a7f8116fbbd4e3145be044
lilongyun <932740514@qq.com>
2018-05-14 10:25:11 +0800

    新增webproject

--- webproject/src/main/webapp/ckeditor/config.js
+++ webproject/src/main/webapp/ckeditor/config.js
@@ -13,10 +13,10 @@ CKEDITOR.editorConfig = function( config ) {
     config.filebrowserImageBrowseUrl ='ckfinder/ckfinder.html?Type=Images';                                          
     // 上传Flash时浏览服务文件夹                                                                                                     
     config.filebrowserFlashBrowseUrl ='ckfinder/ckfinder.html?Type=Flash';                                           
-    // 上传文件按钮(标签)                                                                                                         
-    config.filebrowserUploadUrl ='ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files';       
-    // 上传图片按钮(标签)                                                                                                         
-    config.filebrowserImageUploadUrl ='ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Images';  
-    // 上传Flash按钮(标签)                                                                                                      
-    config.filebrowserFlashUploadUrl ='ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';  
+	// 上传文件按钮(标签) 
+	config.filebrowserUploadUrl ='ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Files';
+	// 上传图片按钮(标签)                                                                                                         
+	config.filebrowserImageUploadUrl ='ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Images';
+	// 上传Flash按钮(标签) 
+	config.filebrowserFlashUploadUrl ='ckfinder/core/connector/java/connector.java?command=QuickUpload&type=Flash';
 };
